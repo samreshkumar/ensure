98f6b6efb202b95e36d9bdf21bbb1c1d4dbfade5
manoj20 <manoj@hsad.co.in>
2021-04-16 09:56:16 -0700

    desktop code done

---
+++ js/custom.js
@@ -0,0 +1,51 @@
+
+ $(document).ready(function() {
+    $('.home-banner-slider .owl-carousel').owlCarousel({
+      loop: true,
+      margin: 0,
+      animateOut: 'fadeOut',
+      autoplay:1500,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true
+        },
+        600: {
+          items: 1,
+          nav: true
+        },
+        1000: {
+          items: 1,
+          nav: true,
+          loop: true
+        }
+      }
+    })
+    $('.client-slider .owl-carousel').owlCarousel({
+      loop: true,
+      margin: 0,
+      animateOut: 'fadeOut',
+      autoplay:1500,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: 1,
+          nav: true
+        },
+        600: {
+          items: 1,
+          nav: true
+        },
+        1000: {
+          items: 1,
+          nav: true,
+          loop: true
+        }
+      }
+    })
+  })
+
+
+
+  
